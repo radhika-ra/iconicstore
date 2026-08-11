3a338f210f768a5824bd8efcad2ef22b1ceaa4b7
radhika-ra <radhikaraveendran98@gmail.com>
2026-08-11 11:45:39 +0530

    Add User entity and login.html page

--- src/main/java/com/radhika/iconicstore/entity/User.java
+++ src/main/java/com/radhika/iconicstore/entity/User.java
@@ -1,4 +1,35 @@
 package com.radhika.iconicstore.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "username"))
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private long mobile;
+
+    @Column(nullable = false)
+    private String emailId;
+
+    @Column(nullable = false)
+    private String role = "CUSTOMER";
 }
